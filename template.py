08ab5b02c5ecf4214be784aa7dcb16ecc6902261
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-05-06 13:37:46 -0400

    Added the draggable class

--- template.py
+++ template.py
@@ -2,23 +2,33 @@
 import pygame
 from polybius.abstractGame import AbstractGame
 from polybius.managers import FRAMES
-from polybius.graphics import Drawable 
+from polybius.graphics import Drawable
+from polybius.utils.draggable import Draggable
 
 class ClubGame(AbstractGame):
 
     def __init__(self):
         AbstractGame.__init__(self, (1000,600), "Game Title")
         FRAMES.prepareImage("image.png", colorKey=True)
-        self._sprite = Drawable("image.png",(100,100))
+        self._sprite = Sprite((100,100))
 
     def draw(self, screen):
         self._sprite.draw(screen)
         
     def handleEvent(self, event):
-        pass
+        self._sprite.handleEvent(event)
 
     def update(self, ticks):
         pass
+
+class Sprite(Drawable, Draggable):
+
+    def __init__(self, position):
+        Drawable.__init__(self, "image.png", position)
+        Draggable.__init__(self)
+
+    def handleEvent(self, event):
+        Draggable.handleDraggingEvent(self, event)
 
 g = ClubGame()
 g.run()
