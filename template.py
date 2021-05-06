e8407cf4ac35814e30f2ab8f5a5f1a3addea1a9a
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-05-06 12:45:42 -0400

    Added a basic sprite to the template

--- template.py
+++ template.py
@@ -1,14 +1,18 @@
 
 import pygame
 from polybius.abstractGame import AbstractGame
+from polybius.managers import FRAMES
+from polybius.graphics import Drawable 
 
 class ClubGame(AbstractGame):
 
     def __init__(self):
         AbstractGame.__init__(self, (1000,600), "Game Title")
+        FRAMES.prepareImage("image.png", colorKey=True)
+        self._sprite = Drawable("image.png",(100,100))
 
     def draw(self, screen):
-        pass
+        self._sprite.draw(screen)
         
     def handleEvent(self, event):
         pass
